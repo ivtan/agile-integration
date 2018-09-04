000cacb618fc84b1587da26a4061b95d52f074fe
ivtan <ivtan@redhat.com>
2018-09-04 15:05:48 +0800

    Fixes

--- src/main/java/org/mycompany/MyTransformer.java
+++ src/main/java/org/mycompany/MyTransformer.java
@@ -25,7 +25,8 @@ import org.springframework.stereotype.Component;
 public class MyTransformer {
 
     public String transform() {
-        // let's return a random string
+        /*
+    	// let's return a random string
         StringBuffer buffer = new StringBuffer();
         for (int i = 0; i < 3; i++) {
             int number = (int) (Math.round(Math.random() * 1000) % 10);
@@ -33,6 +34,10 @@ public class MyTransformer {
             buffer.append(letter);
         }
         return buffer.toString();
+        */
+    	
+    	return "Hello from CustomerInfo API";
+    	
     }
 
 }
